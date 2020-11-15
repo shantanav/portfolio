7c11dc9895743925f5be6de49179103f07e91c39
Shantanav Saurav <ss9415@g.rit.edu>
2020-11-14 23:03:44 -0500

    Recreating the navbar as it was before

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import './App.css'
+import Navbar from './Navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Navbar /> 
   );
 }
 
